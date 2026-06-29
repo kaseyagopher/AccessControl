b6936f5931d1a7ab705e5e9132c5cb9cf7b942cc
kaseyagopher <alghufarsanjab@gmail.com>
2026-06-29 09:14:57 +0100

    ..

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
                     @csrf
 
                     <div>
-                        <label for="login" class="mb-2 block text-sm font-medium text-slate-700">Adresse email</label>
+                        <label for="login" class="mb-2 block text-sm font-medium text-slate-700">Adre</label>
                         <input
                             type="text"
                             name="login"
